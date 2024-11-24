a084c5787a52aaa798c496f0683796affb5d2d32
Quang <nguyenvanquang247@gmail.com>
2024-11-24 15:05:02 +0700

    Add more logs

--- dsst_etl/db.py
+++ dsst_etl/db.py
@@ -1,3 +1,4 @@
+from sqlalchemy.engine.url import make_url
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import create_database, database_exists
 
@@ -14,6 +15,14 @@ def get_db_session(is_test=False):
 
 def init_db(is_test=False):
     engine = get_db_engine(is_test)
+    url = make_url(engine.url)
+    database = url.database
+    dialect_name = url.get_dialect().name
+
+    print("url: ", url)
+    print("database: ", database)
+    print("dialect_name: ", dialect_name)
+
     print("engine.urlengine.urlengine.url: ", engine.url)
     print("database_exists(engine.url): ", database_exists(engine.url))
     if not database_exists(engine.url):
