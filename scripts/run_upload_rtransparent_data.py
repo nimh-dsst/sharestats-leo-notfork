b26bd90d6e1ab484e2403fa8cd7358ad17f9bbd3
Quang <nguyenvanquang247@gmail.com>
2024-12-01 21:13:22 +0700

    remove unused commnets

--- scripts/run_upload_rtransparent_data.py
+++ scripts/run_upload_rtransparent_data.py
@@ -1,22 +1,17 @@
-import logging
 import argparse
 from dsst_etl.db import get_db_session
 from dsst_etl.upload_rtransparent_data import RTransparentDataUploader  
 
 
 def main():
-    # Set up argument parser
     parser = argparse.ArgumentParser(description='Upload RTransparent data from a file.')
     parser.add_argument('input_file', type=str, help='Path to the input file (feather or parquet)')
     
-    # Parse arguments
     args = parser.parse_args()
     
-    # Create an instance of RTransparentDataUploader
     uploader = RTransparentDataUploader(get_db_session())
     
-    # Call the method to upload data with the input file
-    uploader.upload_data(args.input_file)  # Assuming 'upload_data' accepts a file path
+    uploader.upload_data(args.input_file)
 
 if __name__ == "__main__":
     main()
